b86577c0ba14685b0a8841696964e37633dbd7d3
GabBraga <g4b.braga@gmail.com>
2024-10-14 15:01:24 -0300

    Metodo de logar criado na classe Pai

--- lib/classes.dart
+++ lib/classes.dart
@@ -2,8 +2,16 @@ class Funcionario{
   final String userName, senha;
 
   Funcionario(this.userName,this.senha);
+
+  void logar(Map<String,String> BD){
+    if (BD.containsKey(userName.toLowerCase())) {
+      BD[userName.toLowerCase()] == senha ? print('Logado'):print('Senha errada');
+    }else{
+      print('User Name errado');
+    }
+  }
 }
 
-class Manutencao{
-  
+class Manutencao extends Funcionario{
+  Manutencao(String userNome, String senha) :super(userNome,senha);
 }
